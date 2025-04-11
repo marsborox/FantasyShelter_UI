2e0ae37ba1477573560a66b057ce305e195ed112
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-04-11 17:57:40 +0200

    initial commit some UI

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string name;
+    public int level;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
