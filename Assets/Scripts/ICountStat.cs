af7fea092137c651cfdc88c8ed1d78cf4aad30de
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-05-03 15:57:59 +0200

    tried stuff in interface w T type

--- Assets/Scripts/ICountStat.cs
+++ Assets/Scripts/ICountStat.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public interface ICountStat
 {
     
-    public void CountStat(ref int globalStat, List<int> listWeCountIn)
+    public void CountStat<T>(ref int globalStat, List<T> listWeCountIn)
     {
         globalStat = 0;
+        //var returnStat;
         
         for (int i = 0; i < listWeCountIn.Count - 1; i++)
-        { 
-            
+        {
+            //globalStat = globalStat + listWeCountIn[i]/*.stat*/;
         }
     }
        
