4700c235685a753768d8af0daae9048b51f7ae4c
marsborox <mierkamaros@gmail.com>
2025-05-03 21:32:03 +0200

    pitzful attempt

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
 
+
 public class Item : MonoBehaviour
 {
+    public string slot;
+    public int health;
+    public int attack;
+    public int defense;
+    public int energy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Scripts/Units/HeroInventoy.cs
+++ Assets/Scripts/Units/HeroInventoy.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class HeroInventoy : MonoBehaviour
 {
+    Item headSlot;
+    Item chestSlot;
+    Item weaponSlot;
+    Item offhandSlot;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +20,44 @@ public class HeroInventoy : MonoBehaviour
     {
         
     }
+
+    void CheckSlot(Item item)
+    {
+        switch (item.slot) 
+        {
+            case "headSlot":
+                PutOnItem(item,ref headSlot);
+                break;
+            case "chestSlot":
+                PutOnItem(item, ref chestSlot);
+                break;
+            case "weaponSlot":
+                PutOnItem(item, ref weaponSlot);
+                break;
+            case "offHandSlot":
+                PutOnItem(item, ref offhandSlot);
+                break;
+             default:
+                Debug.Log("heroInventory. item wrong slot");
+                break;
+        }
+    }
+
+    void PutOnItem(Item item, ref Item slot)
+    {
+        if (slot == null)
+        { slot = item; }
+        else
+        { 
+            //empty slo to inventory
+            slot = item;
+        }
+
+
+    }
+
+
+
+
 }
+ 
