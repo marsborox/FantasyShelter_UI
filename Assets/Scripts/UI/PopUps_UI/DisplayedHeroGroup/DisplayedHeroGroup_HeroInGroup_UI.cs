0f64824cf178fcea16ce1314241490fbdafecf65
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-05-28 23:32:00 +0200

    work on herogroup UI

---
+++ Assets/Scripts/UI/PopUps_UI/DisplayedHeroGroup/DisplayedHeroGroup_HeroInGroup_UI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DisplayedHeroGroup_HeroInGroup_UI : UI
+{
+    [SerializeField] TextMeshProUGUI _heroName;
+    [SerializeField] TextMeshProUGUI _heroRole;
+    [SerializeField] TextMeshProUGUI _heroLvl;
+    [SerializeField] TextMeshProUGUI _heroHealth;
+    [SerializeField] TextMeshProUGUI _heroAttack;
+    [SerializeField] TextMeshProUGUI _heroDefense;
+    [SerializeField] TextMeshProUGUI _heroEnergy;
+    [SerializeField] Button _kickHeroButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
