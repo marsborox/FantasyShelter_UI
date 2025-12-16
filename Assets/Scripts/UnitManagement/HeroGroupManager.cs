7a3606c71503d3f0368ae982066eec772b9acb34
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-12-16 22:56:35 +0100

    checking status of project

--- Assets/Scripts/UnitManagement/HeroGroupManager.cs
+++ Assets/Scripts/UnitManagement/HeroGroupManager.cs
@@ -12,7 +12,7 @@ public class HeroGroupManager : MonoBehaviour
         idCoutner++;
         return idCoutner;
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
 
     /*
     public void CreateGroup()
